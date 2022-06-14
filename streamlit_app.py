ec7bd41068c9eb610c68ccb52a35970a1e2d7df8
Hyly <107153601+blckchar@users.noreply.github.com>
2022-06-14 08:24:39 -0600

    Update streamlit_app.py
    To make it easier to switch out the hard get request, we broke the URL string into the main URL and then the indexed get.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # this line just writes the data directly to the screen with no formatting
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
+
 
 # this will take the json version of the reponse and normalize it to look like a nice table
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
